77818977cdad277038e19d14f8c8f6f559a637e8
zeine712342@gmail.com <zeine712342@gmail.com>
2026-07-26 18:21:49 +0000

    Ingest data to bronze layer

---
+++ src/config.py
@@ -0,0 +1,14 @@
+RAW_PATH = "/Volumes/ecommerce/source/raw"
+BRONZE_PATH = "/Volumes/ecommerce/bronze"
+
+TABLES = {
+    "customers": "customers.csv",
+    "geolocation": "geolocation.csv",
+    "order_items": "order_items.csv",
+    "orders": "orders.csv",
+    "payments": "payments.csv",
+    "product_category_name_translation": "product_category_name_translation.csv",
+    "products": "products.csv",
+    "reviews": "reviews.csv",
+    "sellers": "sellers.csv",
+}
---
+++ src/ingestion.py
@@ -0,0 +1,20 @@
+from config import RAW_PATH, BRONZE_PATH, TABLES
+
+raw_path = RAW_PATH
+bronze_path = BRONZE_PATH
+
+tables = TABLES
+
+for name, file in tables.items():
+    input_path = f"{raw_path}/{file}"
+    output_path = f"{bronze_path}/{name}"
+    print(f'✅ {name} start')
+    df = (
+        spark.read
+        .option('header',True)
+        .option('inferschema',True)
+        .csv(input_path)
+    )
+    df.write.format("delta").mode("overwrite").saveAsTable(f"ecommerce.bronze.{name}")
+    
+    print(f'✅ {name} end')
