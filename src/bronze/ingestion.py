cf4264e50d484f51c0f09a697c274d8d8a84971d
zeine712342@gmail.com <zeine712342@gmail.com>
2026-07-29 14:44:36 +0000

    Fix ingestion bug caused by separator

--- src/bronze/ingestion.py
+++ src/bronze/ingestion.py
@@ -1,20 +1,30 @@
-from src.config import RAW_PATH, BRONZE_PATH, TABLES
+from src.config import RAW_PATH, TABLES
 
 raw_path = RAW_PATH
-bronze_path = BRONZE_PATH
-
 tables = TABLES
 
 for name, file in tables.items():
     input_path = f"{raw_path}/{file}"
-    output_path = f"{bronze_path}/{name}"
-    print(f'✅ {name} start')
+
+    print(f"✅ {name} start")
+
     df = (
         spark.read
-        .option('header',True)
-        .option('inferschema',True)
+        .option("header", True)
+        .option("inferSchema", True)
+        .option("multiLine", True)
+        .option("quote", '"')
+        .option("escape", '"')
+        .option("mode", "PERMISSIVE")
         .csv(input_path)
     )
-    df.write.format("delta").mode("overwrite").saveAsTable(f"ecommerce.bronze.{name}")
+
+    (
+        df.write
+        .format("delta")
+        .mode("overwrite")
+        .saveAsTable(f"ecommerce.bronze.{name}")
+    )
+
+    print(f"✅ {name} end")
     
-    print(f'✅ {name} end')
